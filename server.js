d287078bb1e18df67ef34cbdbff72440368a88c4
Majenayu <vvce23cseaiml0100@vvce.ac.in>
2025-03-28 21:42:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const multer = require("multer");
-const Cadet = require('./models/Cadet'); // Import Cadet model
 const bcrypt = require("bcryptjs");
 
 
